b380dbdfb3dadaef5d6db1733829dd184a157361
lsmman <lsmman07@gmail.com>
2022-02-08 01:52:00 +0900

    Init project

---
+++ src/main/java/com/lsmman/limshpay/LimshpayApplication.java
@@ -0,0 +1,13 @@
+package com.lsmman.limshpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimshpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimshpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsmman/limshpay/LimshpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsmman.limshpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimshpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
